25ff771840bb1b4607f6cdfa0e99867c4ac78d79
jose <joseluis19963@gmail.com>
2020-11-20 19:12:54 -0500

    formulario

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -8,12 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class FormularioComponent implements OnInit {
 
   public title: string;
+  public user: any;
+  public campo: string;
 
   constructor() {
-    this.title = 'Formularios';
+    this.title = 'Formulario';
+    this.user = {
+      nombre: '',
+      apellidos: '',
+      bio: '',
+      genero: ''
+    };
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    alert('Formulario enviado');
+  }
+
+  hasDadoClick() {
+    alert('Has dado click');
+  }
+
+  hasSalido() {
+    alert('Has salido!!!');
+  }
+
+  hasDadoEnter() {
+    alert('Has dado enter');
   }
 
 }

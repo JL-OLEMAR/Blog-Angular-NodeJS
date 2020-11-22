71460a4a7ba69bb60948780e46bfaa23c35f11f6
jose <joseluis19963@gmail.com>
2020-11-22 00:24:06 -0500

    search article

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ArticleService } from '../../services/article.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
+  providers: [ArticleService]
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  public searchString: string;
+
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _articleService: ArticleService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  goSearch() {
+    this._router.navigate(['/buscar', this.searchString]);
   }
 
 }

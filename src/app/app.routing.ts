0dbec929af7f92fd1408d579f4284f854e710426
jose <joseluis19963@gmail.com>
2020-11-22 01:55:17 -0500

    new article

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 // Importar componentes a los cuales les quiero una pagina exclusiva
 import { HomeComponent } from './components/home/home.component';
 import { BlogComponent } from './components/blog/blog.component';
+import { ArticleNewComponent } from './components/article-new/article-new.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
 import { PaginaComponent } from './components/pagina/pagina.component';
@@ -12,12 +13,14 @@ import { ArticleComponent } from './components/article/article.component';
 import { SearchComponent } from './components/search/search.component';
 import { ErrorComponent } from './components/error/error.component';
 
+
 // Array de rutas
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'blog', component: BlogComponent },
   { path: 'blog/articulo/:id', component: ArticleComponent },
+  { path: 'blog/crear', component: ArticleNewComponent },
   { path: 'buscar/:search', component: SearchComponent },
   { path: 'formulario', component: FormularioComponent },
   { path: 'peliculas', component: PeliculasComponent },
---
+++ src/app/components/article-new/article-new.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Article } from '../../models/article';
+import { ArticleService } from '../../services/article.service';
+
+@Component({
+  selector: 'app-article-new',
+  templateUrl: './article-new.component.html',
+  styles: [],
+  providers: [ArticleService]
+})
+export class ArticleNewComponent implements OnInit {
+
+  public article: Article;
+  public status: string;
+
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _articleService: ArticleService
+  ) {
+    this.article = new Article('', '', '', null, null);
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this._articleService.create(this.article).subscribe(
+      response => {
+        if (response.status == 'success') {
+          this.status = 'success';
+          this.article = response.article;
+          this._router.navigate(['/blog']);
+        } else {
+          this.status = 'error';
+        }
+      },
+      error => {
+        console.log(error);
+        this.status = 'error';
+      }
+    );
+  }
+
+}
